c4f1d270920dc072654b572d564507bdab756ca4
Flerpaderp <liv.cheesypeach@gmail.com>
2022-11-14 18:55:38 -0600

    uploading homework

--- Assignments/propertychange.py
+++ Assignments/propertychange.py
@@ -29,3 +29,40 @@ GIT COMMANDS TO SAVE FILES
 5. git commit -m "message for commit"
 6. git push - sends the data to the remote repository
 '''
+
+import random
+import os 
+
+clear = lambda: os.system('clear')
+clear()
+propertiesList = { 1 : "Poison Resist" ,  2 : "Strength" , 3 : "Walking Speed" , 4 : "Armor" , 
+5 : "Vitality" , 6 : "Mana Siphon" , 7 : "Critical Hit Chance" , 8 : "Critical Hit Damage" , 9 : "Vitality Siphon" }
+
+def propertyValue():
+     number = random.randint(50, 1500)
+     return number
+
+def giveProperty():
+    value = random.choice(list(propertiesList.values()))
+    return value
+
+print ("Here are your initial values:")
+
+attribute = giveProperty()
+points = propertyValue()
+skillsDictonary = {1:(str(giveProperty())) + " " + str(propertyValue()), 2: (str(giveProperty()) + " " + str(propertyValue())), 3: (str(giveProperty()) + " " + str(propertyValue()))}
+print(skillsDictonary)
+
+print("Are you happy with your selection?")
+satisfied = input()
+if satisfied == 'Y' or satisfied == 'y':
+    quit()
+if satisfied == 'N' or satisfied == 'n':
+    print("Which Selection Would You Change?")
+    choice = int(input())
+    clear()
+    print("Here is your updated list:")
+    skillsDictonary.update({choice:(str(giveProperty())) + " " + str(propertyValue())})
+    print(skillsDictonary)
+
+
